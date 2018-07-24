519a719b00bd0b33777569d1a0347952281d4790
Your Name <euy.cho@gmail.com>
2018-07-24 13:33:35 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("hello");
+        // operators
+        Operators operators = new Operators();
+        operators.calculate();
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+    public void calculate() {
+        System.out.println(1 + 2);
+        System.out.println(3 - 1);
+        System.out.println(1 * 2);
+        System.out.println(4 / 2);
+
+        // special case
+        System.out.println(3 / 2);
+        System.out.println(3 % 2);
+    }
+}
